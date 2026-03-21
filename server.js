34f8b89ecfdb0ced0f236c3918ec4722a6c527d4
sellsabilrahma <157852387+sellsabilrahma@users.noreply.github.com>
2026-03-21 21:03:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-import connectDB from "./DBconnect.js"
+import connectDB from "./DBconnect.js";
 import pharmacieRouter from "./pharmacieRouter.js";
 
 config();
 
-const app = express()
-const port = process.env.PORT || https://dawa-backend.onrender.com
+const app = express();
 
-connectDB(process.env.DATABASE_NAME)
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"));
+
+connectDB(process.env.MONGO_URI);
+
+app.use(express.json());
+app.use(cors());
 
 app.use("/api", pharmacieRouter);
 
 app.get('/', (req, res) => {
-    res.send("API IS WORKING ")
-})
+    res.send("API IS WORKING");
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
